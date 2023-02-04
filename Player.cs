3f7f5d24bc1adb6cf2de3fdc172337e59ba31598
alexandersimmons0 <122643996+alexandersimmons0@users.noreply.github.com>
2023-02-03 17:46:17 -0700

    Add files via upload

--- Player.cs
+++ Player.cs
@@ -4,14 +4,19 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public float speed = 20f;
+    public float speed = 10f;
     public float rotation = 75f;
+    public float jump = 5f;
+    public float distanceToGround = 0.1f;
+    public LayerMask groundLayer;
     private float vInput;
     private float hInput;
     private Rigidbody _rb;
+    private CapsuleCollider _col;
 
     void Start(){
         _rb = GetComponent<Rigidbody>();
+        _col = GetComponent<CapsuleCollider>();
     }
     void Update()
     {
@@ -22,9 +27,21 @@ public class Player : MonoBehaviour
         this.transform.Rotate(Vector3.up * hInput * Time.deltaTime);    */
     }
     void FixedUpdate(){
+        if(IsGrounded() && Input.GetKeyDown(KeyCode.Space)){
+            _rb.AddForce(Vector3.up * jump, ForceMode.Impulse);
+        }
         Vector3 rotation = Vector3.up * hInput;
         Quaternion angleRot = Quaternion.Euler(rotation * Time.deltaTime);
         _rb.MovePosition(this.transform.position + this.transform.forward * vInput * Time.fixedDeltaTime);
         _rb.MoveRotation(_rb.rotation * angleRot);
     }
+    private bool IsGrounded(){
+        Vector3 capsuleBottom = new Vector3(_col.bounds.center.x,
+        _col.bounds.min.y, _col.bounds.center.z);
+        bool grounded = Physics.CheckCapsule(_col.bounds.center,
+        capsuleBottom, distanceToGround, groundLayer,
+        QueryTriggerInteraction.Ignore);
+        return grounded;
+ }
+
 }
